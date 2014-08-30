487e745870393102178d0b7799de7f817710ca57
Tom Hicks <headhunter3@gmail.com>
2014-08-29 21:46:50 -0700

    Updates pom to build with newer versions of dependencies. Removes an unneeded import.

--- Metropolis/src/main/java/com/majinnaibu/bukkitplugins/metropolis/commands/MetropolisHomeEvictCommand.java
+++ Metropolis/src/main/java/com/majinnaibu/bukkitplugins/metropolis/commands/MetropolisHomeEvictCommand.java
@@ -4,7 +4,6 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import com.majinnaibu.bukkitplugins.metropolis.MetropolisPlugin;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
